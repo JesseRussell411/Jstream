10ce7b716e20370f83f1c5da10d626f7211af815
jesse russell <jesserussell411@gmail.com>
2022-12-18 19:28:11 -0700

    made awaitable a bit fuzzier

--- src/types/async.d.ts
+++ src/types/async.d.ts
@@ -1,5 +1,17 @@
-/** Either a {@link Promise} of the given type or just the given type. */
-export type Awaitable<T> = T | Promise<T>;
+/** Either a {@link Promise} of the given type or just the given type or a promise chain up to a depth of 8 of type T.*/
+export type Awaitable<T> =
+    | T
+    | Promise<T>
+    | Promise<Promise<T>>
+    | Promise<Promise<Promise<T>>>
+    | Promise<Promise<Promise<Promise<T>>>>
+    | Promise<Promise<Promise<Promise<Promise<T>>>>>
+    | Promise<Promise<Promise<Promise<Promise<Promise<T>>>>>>
+    | Promise<Promise<Promise<Promise<Promise<Promise<Promise<T>>>>>>>
+    | Promise<Promise<Promise<Promise<Promise<Promise<Promise<Promise<T>>>>>>>>;
+
+// * tried this, didn't work ):
+// export type Awaitable<T> = T | Promise<Awaitable<T>>
 
 /** Either an {@link Iterable} or an {@link AsyncIterable} */
 export type AwaitableIterable<T> = Iterable<T> | AsyncIterable<T>;
