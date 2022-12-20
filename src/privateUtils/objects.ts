a5d18cb9312902619366fdf7f29d0f16e396d964
jesse russell <jesserussell411@gmail.com>
2022-12-20 02:49:44 -0700

    fixed a type error

--- src/privateUtils/objects.ts
+++ src/privateUtils/objects.ts
@@ -1,6 +1,6 @@
 export function getOwnPropertyKeys<K extends keyof any>(
     object: Record<K, any>
-): K & (string | symbol)[] {
+): (K & (string | symbol))[] {
     return [
         ...Object.getOwnPropertyNames(object),
         ...Object.getOwnPropertySymbols(object),
@@ -12,6 +12,9 @@ export function getOwnEntries<K extends keyof any, V>(
 ): [K & (string | symbol), V][] {
     return [
         ...Object.getOwnPropertyNames(object).map(n => [n, (object as any)[n]]),
-        ...Object.getOwnPropertySymbols(object).map(s => [s, (object as any)[s]]),
+        ...Object.getOwnPropertySymbols(object).map(s => [
+            s,
+            (object as any)[s],
+        ]),
     ] as any;
 }
